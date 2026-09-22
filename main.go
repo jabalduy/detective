19dcab3d4468d97c86b2b9e04c3c3750c31d607d
jabalduy <burbagaa@gmail.com>
2026-09-22 15:23:55 +0300

    убрал терминальную часть

--- main.go
+++ main.go
@@ -1,14 +1,5 @@
 package main
 
 func main() {
-	// clues, err := LoadClues("cases/case_017/clues.json")
-	// if err != nil {
-	// 	panic(err)
-	// }
-
-	// for _, clue := range clues {
-	// 	fmt.Println(clue.Name, clue.ObjID, clue.Key, clue.Found)
-	// }
-
 	StartServer()
 }
